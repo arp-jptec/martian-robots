c50263f44dd25be2dbd36059c28e415606fefa7c
arp <arp.iitm@gmail.com>
2020-07-29 22:18:12 +0100

    ARP|Implementation for command instruction to move robot

--- MartianRobots.java
+++ MartianRobots.java
@@ -2,9 +2,53 @@ public class MartianRobots {
 
     private int[][] grid;
 
-    private MartianRobots(int upperX, int upperY){
+    public MartianRobots(int upperX, int upperY){
         grid = new int[++upperX][++upperY];
     }
+
+    public void instruct(RobotPosition robotPosition, String instruction){
+        for(char command : instruction.toCharArray()){
+           if(!canMove(robotPosition,command))
+               return;
+        }
+        System.out.println(robotPosition);
+    }
+
+    private boolean canMove(RobotPosition robotPosition, char command){
+        switch (command){
+            case 'L':
+                robotPosition.direction = robotPosition.direction.left;
+                return true;
+            case 'R':
+                robotPosition.direction = robotPosition.direction.right;
+                return true;
+            case 'F': {
+                int nextX = robotPosition.x + robotPosition.direction.xMove;
+                int nextY = robotPosition.y + robotPosition.direction.yMove;
+                try{
+                    grid[nextX][nextY];
+                    robotPosition.x = nextX;
+                    robotPosition.y = nextY;
+                    return true;
+                }catch (ArrayIndexOutOfBoundsException aie){
+                    if(grid[robotPosition.x][robotPosition.y] == 0){
+                        System.out.print(robotPosition);
+                        System.out.println(" LOST");
+                        //Leave scent (-1) when robot lost
+                        grid[robotPosition.x][robotPosition.y] = -1;
+                        return false;
+                    }
+                    else
+                        return true;
+
+                }
+            }
+            default:
+                throw new IllegalArgumentException("Invald instruction")
+
+        }
+    }
+
 
     class RobotPosition{
         int x;
@@ -16,8 +60,11 @@ public class MartianRobots {
             this.y = y;
             this.direction = direction;
         }
-    }
 
+        public String toString(){
+            return x+" "+y+" "+direction.label;
+        }
+    }
 
     enum Direction {
         NORTH('N',WEST,EAST,0,1),
